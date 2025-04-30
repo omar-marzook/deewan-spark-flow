154ee2cd3fc6c87c118e18d942f10c29c6e9fe31
Omar Marzouq <omarmarzook92@gmail.com>
2025-04-30 12:15:48 +0300

    remove background in product page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -39,7 +39,7 @@ export default function ProductPage() {
   };
 
   return (
-    <div className="overflow-x-hidden bg-white text-deewan-dark">
+    <div className="overflow-x-hidden">
       <Navbar />
       <ProductHero 
         name={productData.name} 
