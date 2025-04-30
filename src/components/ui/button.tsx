646f629a95d68e1be81eefd6fb66a2f0457a9c85
Omar Marzouq <omarmarzook92@gmail.com>
2025-04-30 16:13:33 +0300

    edit button comp style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,25 +5,28 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-deewan-primary text-white rounded-md shadow-md hover:bg-deewan-primary/90 transition-all duration-300 hover:-translate-y-1",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-white text-black rounded-md border border-deewan-secondary/20 shadow-sm hover:shadow-md transition-all duration-300 hover:-translate-y-1",
+        accent:
+          "bg-deewan-accent text-deewan-dark rounded-md shadow-md hover:shadow-lg transition-all duration-300 hover:-translate-y-1",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: "px-4 py-2 text-base",
+        sm: "rounded-md px-3 text-sm",
+        lg: "rounded-md px-8 text-md py-3 px-6",
+        xl: "rounded-md px-8 text-lg py-3 px-6",
+        icon: "h-10 w-10 text-base",
       },
     },
     defaultVariants: {
@@ -35,7 +38,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
