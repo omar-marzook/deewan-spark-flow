773c38fa912a1eda834859f8eb37d5eb392a7216
Omar Marzouq <omarmarzook92@gmail.com>
2025-05-08 16:16:05 +0300

    add social links

--- src/components/footer/SocialLinks.tsx
+++ src/components/footer/SocialLinks.tsx
@@ -6,19 +6,10 @@ import { cn } from "@/lib/utils";
 const SocialLinks = () => {
   return (
     <div className="flex space-x-3">
-      <a href="#" aria-label="Youtube" className={cn("inline-flex items-center justify-center rounded-full p-2.5", "bg-white/50 backdrop-blur-sm text-deewan-primary", "hover:bg-deewan-primary/20 transition-colors")}>
-        <Youtube size={18} />
-      </a>
-      <a href="#" aria-label="Facebook" className={cn("inline-flex items-center justify-center rounded-full p-2.5", "bg-white/50 backdrop-blur-sm text-deewan-primary", "hover:bg-deewan-primary/20 transition-colors")}>
-        <Facebook size={18} />
-      </a>
-      <a href="#" aria-label="Twitter" className={cn("inline-flex items-center justify-center rounded-full p-2.5", "bg-white/50 backdrop-blur-sm text-deewan-primary", "hover:bg-deewan-primary/20 transition-colors")}>
+      <a href="https://x.com/DeewanKSA" target="_blank" aria-label="X" className={cn("inline-flex items-center justify-center rounded-full p-2.5", "bg-deewan-primary/10 backdrop-blur-sm text-deewan-primary", "hover:bg-deewan-primary/20 transition-colors")}>
         <Twitter size={18} />
       </a>
-      <a href="#" aria-label="Instagram" className={cn("inline-flex items-center justify-center rounded-full p-2.5", "bg-white/50 backdrop-blur-sm text-deewan-primary", "hover:bg-deewan-primary/20 transition-colors")}>
-        <Instagram size={18} />
-      </a>
-      <a href="#" aria-label="LinkedIn" className={cn("inline-flex items-center justify-center rounded-full p-2.5", "bg-white/50 backdrop-blur-sm text-deewan-primary", "hover:bg-deewan-primary/20 transition-colors")}>
+      <a href="https://www.linkedin.com/company/deewanks" target="_blank" aria-label="LinkedIn" className={cn("inline-flex items-center justify-center rounded-full p-2.5", "bg-deewan-primary/10 backdrop-blur-sm text-deewan-primary", "hover:bg-deewan-primary/20 transition-colors")}>
         <Linkedin size={18} />
       </a>
     </div>
